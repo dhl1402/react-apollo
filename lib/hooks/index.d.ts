07c4e9d1a1dfdab18ed6b679a5b770eb7735042e
DHL <dhl1402@gmail.com>
2018-11-21 13:57:34 +0700

    lib

---
+++ lib/hooks/index.d.ts
@@ -0,0 +1,2 @@
+export { default as Query } from './Query';
+export { default as useQuery } from './useQuery';
---
+++ lib/hooks/useQuery.d.ts
@@ -0,0 +1,4 @@
+import { OperationVariables } from '../types';
+import { QueryProps } from './Query';
+declare const _default: (props: QueryProps<any, OperationVariables>) => import("./Query").QueryResult<any, OperationVariables>;
+export default _default;
